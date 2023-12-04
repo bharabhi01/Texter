8bfb903d6f60286345a7482d1ef6ea2d1df5659b
Abhishek Bhardwaj <bhardwajabhi2701@gmail.com>
2023-12-05 01:12:01 +0530

    Created a websocket server and also created a basic connection between the frontend client and the backend server using websockets.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect, sendMessage } from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Hello World!</div>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    connect();
+  }
+
+  send() {
+    console.log("Hi!")
+    sendMessage("Hi!")
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.send}>Send Message</button>
+      </div>
+    )
+  }
 }
 
 export default App;
